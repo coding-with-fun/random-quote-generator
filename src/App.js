a7a08a7898cd469dd6bc13c29ec1290e1964a7bb
Harsh Patel <harshp2482@gmail.com>
2020-07-19 19:03:10 +0530

    Fetched quotes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [quotes, setQuotes] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [index, setIndex] = useState(quotes.length);
+
+	const fetchQuote = async () => {
+		setLoading(true);
+		const data = await Axios.get("https://api.quotable.io/random");
+
+		setQuotes([
+			...quotes,
+			{
+				author: data.data.author,
+				content: data.data.content,
+			},
+		]);
+		setIndex(quotes.length);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		fetchQuote();
+		// eslint-disable-next-line
+	}, []);
+
+	return (
+		<div className="App">
+			{loading ? (
+				<p>Loading...</p>
+			) : (
+				<>
+					{quotes[index] ? (
+						<>
+							<p>{quotes[index].content}</p>
+							<p>{quotes[index].author}</p>
+							<p>{index}</p>
+						</>
+					) : (
+						<p>{loading}</p>
+					)}
+					<button
+						disabled={index === 0 ? true : false}
+						onClick={() => setIndex(index - 1)}
+					>
+						Prev
+					</button>
+					<button onClick={fetchQuote}>Get New Quote</button>
+					<button
+						disabled={index === quotes.length - 1 ? true : false}
+						onClick={() => setIndex(index + 1)}
+					>
+						Next
+					</button>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
